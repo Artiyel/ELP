2c076130f9ec968214cd9eab66631b7b9eff7a98
rainbowsdy <alexisrizza1@gmail.com>
2026-01-14 09:54:29 +0100

    heuuuuu j'ai cassé wls

--- ELM/Alexis/src/Test.elm
+++ ELM/Alexis/src/Test.elm
@@ -12,11 +12,7 @@ dupli liste =
 compress liste =
     case liste of
     [] -> []
-    numero1::xs ->
-        if List.length xs > 2
-            then case xs of 
-                numero2::xss -> if numero1==numero2 then compress numero2::xss else compress xss
-                [a,b] ->  if numero1==numero2 then compress numero2::xss else compress xss
-        else if numero1==xs then [xs]
-        else if xs == [] then []
-        else xs
+    [x]->[x]
+    [a::b::xs]->
+        if a==b then compress (b::xs)
+        else compress a :: compress (b::xs)

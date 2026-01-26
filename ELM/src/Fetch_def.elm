e56ebc7f8256987fb44c3380f0ee336c49e7ab73
rainbowsdy <alexisrizza1@gmail.com>
2026-01-26 09:52:13 +0100

    added petit commentaire en elp

--- ELM/src/Fetch_def.elm
+++ ELM/src/Fetch_def.elm
@@ -3,21 +3,15 @@ module Fetch_def exposing (getdef, Msg_2(..), Definition)
 import Http
 import Json.Decode exposing (Decoder, map2, map3, map, field, string, list)
 
+ -- Definitions de la structure de donnée utilisée
 type Msg_2 = Trouvé (Result Http.Error (List Definition)) | Rien
-
-
-getdef : String -> Cmd Msg_2
-getdef mot =
-  Http.get
-    { url = "https://api.dictionaryapi.dev/api/v2/entries/en/" ++ mot
-    , expect = Http.expectJson Trouvé definitionsDecoder
-    }
 
 type alias Definition =
     { mot : String
     , meanings : List Meaning
     , prononciation : String
     }
+
 
 type alias Meaning =
     { partOfSpeech : String
@@ -26,26 +20,19 @@ type alias Meaning =
 
 
 -- COMMANDE HTTP
-getDef : String -> Cmd Msg_2
-getDef mot =
+getdef : String -> Cmd Msg_2
+getdef mot =
     Http.get
         { url = "https://api.dictionaryapi.dev/api/v2/entries/en/" ++ mot
         , expect = Http.expectJson Trouvé definitionsDecoder
         }
 
 
--- DECODERS
-definitionTextDecoder : Decoder String
-definitionTextDecoder =
-    field "definition" string
+-- DECODERS dans "l'ordre" d'appel
 
-
-meaningDecoder : Decoder Meaning
-meaningDecoder =
-    map2 Meaning
-        (field "partOfSpeech" string)
-        (field "definitions" (list definitionTextDecoder))
-
+definitionsDecoder : Decoder (List Definition)
+definitionsDecoder =
+    list definitionDecoder
 
 definitionDecoder : Decoder Definition
 definitionDecoder =
@@ -55,6 +42,18 @@ definitionDecoder =
         (field "phonetic" string)
 
 
-definitionsDecoder : Decoder (List Definition)
-definitionsDecoder =
-    list definitionDecoder
+meaningDecoder : Decoder Meaning
+meaningDecoder =
+    map2 Meaning
+        (field "partOfSpeech" string)
+        (field "definitions" (list definitionTextDecoder))
+
+definitionTextDecoder : Decoder String
+definitionTextDecoder =
+    field "definition" string
+
+
+
+
+
+
